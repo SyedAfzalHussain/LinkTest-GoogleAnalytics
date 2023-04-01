d6c600ea30c46ea71cbffd7cd8692e2b1d1ab9ff
Afzal Kazmi <kazmiafzal348@gmail.com>
2023-04-01 20:51:09 +0500

    Working

--- index.js
+++ index.js
@@ -18,33 +18,58 @@ app.get('/', (req, res) => {
 app.post('/getUrl', (req, res) => {
 
     const url1 = req.body.url;
-    var variable1 , variable;
+    var variable = '';
     fetch(url1)
         .then(response => response.text())
         .then(html => {
-            if (html.includes('gtm.js')) {
-                variable = 'Google Search Console is enabled on this website</br>';
+            if (html.includes('gtm.js') && html.includes('googletagmanager.com')) {
+                variable += `Google tag Manager is enabled on this website</br>`;
             } else {
-                variable = 'Google Search Console is not enabled on this website</br>';
+                variable += `Google tag Manager is not enabled on this website</br>`;
             }
-            console.log("Afzal Kazmi is the King")
-
-        })
-        .catch(error => console.error(error));
-        fetch(url1)
-        .then(response => response.text())
-        .then(html => {
-            if (html.includes('googletagmanager')) {
-                variable1 = 'Google  is enabled on this website</br>';
-            } else {
-                variable1 = `Google Tag Manager is not enabled on this website</br>`;
-            }
-            variable +=variable1;
             res.send({ "ans": variable });
         })
         .catch(error => console.error(error));
 }
 );
+
+app.post('/getGTM', (req, res) => {
+    console.log("getGTM")
+    const url1 = req.body.url;
+    var variable = '';
+    fetch(url1)
+        .then(response => response.text())
+        .then(html => {
+            if (html.includes('google-my-business')) {
+                variable += `Google My Business is enabled on this website</br>`;
+            } else {
+                variable += `Google My Business is not enabled on this website</br>`;
+            }
+            res.send({ "ans": variable });
+        })
+        .catch(error => console.error(error));
+
+});
+
+app.post('/getDomain', (req, res) => {
+    console.log("getDomain")
+    const url1 = req.body.url;
+    var variable = '';
+    fetch(url1)
+        .then(response => response.text())
+        .then(html => {
+            if (html.includes(`google-site-verification`)) {
+                variable += `Google Search Console is enabled on this website</br>`;
+            } else {
+                variable += `Google Search Console is not enabled on this website</br>`;
+            }
+            console.log(variable)
+            res.send({ "ans": variable });
+        })
+        .catch(error => console.error(error));
+}
+);
+
 
 //start the server
 
