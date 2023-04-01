39169f08962b4a8e663b5b9c79327e4179a3d41c
Afzal Kazmi <kazmiafzal348@gmail.com>
2023-04-01 20:58:30 +0500

    Envirnment Port

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 //create a route for the app
